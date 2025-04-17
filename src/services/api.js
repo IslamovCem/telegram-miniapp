7e1fd5fd31a9b989b05414f00028790f336ccfa3
IslamovCem <126634566+IslamovCem@users.noreply.github.com>
2025-04-17 11:37:41 +0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,6 @@ const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 export async function getProducts() {
   const res = await fetch(`${API_BASE}/api/products`);
+  if (!res.ok) throw new Error("API xatolik");
   return res.json();
 }
